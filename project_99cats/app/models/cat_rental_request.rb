20fc91742d3e2751bd7f4f9ca5186499e2ddead1
Ken <kchansf5@gmail.com>
2018-05-08 17:22:23 -0700

    Create custom validation #does_not_overlap_approved_request

--- project_99cats/app/models/cat_rental_request.rb
+++ project_99cats/app/models/cat_rental_request.rb
@@ -2,6 +2,7 @@ class CatRentalRequest < ApplicationRecord
   validates :status, inclusion: { in: %w(PENDING APPROVED DENIED),
     message: "%{value} is not a status" }
   validates :cat_id, :start_date, :end_date, :status, presence: true
+  validate :does_not_overlap_approved_request
 
   belongs_to :cat,
     primary_key: :id,
@@ -9,13 +10,20 @@ class CatRentalRequest < ApplicationRecord
     class_name: "Cat"
 
   def overlapping_requests
-
     CatRentalRequest
       .where.not(id: self.id)
       .where(cat_id: self.cat_id,
         start_date: (self.start_date..self.end_date).to_a)
-      .or(CatRentalRequest.where.not(id: self.id).and(CatRentalRequest.where(end_date: (self.start_date..self.end_date).to_a)))
-
-
+      .or(CatRentalRequest.where.not(id: self.id)
+        .where(end_date: (self.start_date..self.end_date).to_a))
   end
+
+  def overlapping_approved_requests
+    self.overlapping_requests.where(status: "APPROVED")
+  end
+
+  def does_not_overlap_approved_request
+    !self.overlapping_approved_requests.exists?
+  end
+
 end
